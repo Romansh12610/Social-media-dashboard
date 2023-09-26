70fe96c36dfe70bbc496c5bbb86f11a40d50c797
RomanSh <romashmarov82@gmail.com>
2023-09-27 02:36:32 +0300

    add some event-handlers for toggle

--- app/js/script.js
+++ app/js/script.js
@@ -4,36 +4,119 @@ console.log("HELLO!");
 let wrapper = document.querySelector('.toggle__wrapper');
 let btnBg = document.querySelector('.toggle__background');
 let btn = document.querySelector('.toggle__button');
-let initShift = btn.getBoundingClientRect().left - btnBg.getBoundingClientRect().left;
+let shiftX = btn.getBoundingClientRect().left - btnBg.getBoundingClientRect().left;
 
-wrapper.addEventListener('click', toggleTheme);
+btn.addEventListener('click', toggleTheme);
 
 function toggleTheme(event) {
-    console.log(initShift);
-    let newLeft;
-
+    if (event.target.tagName == 'INPUT') return;
     //changing checked radio btn
     let checkedBtn = document.querySelector('input[checked]');
+
     if (checkedBtn.id === 'dark') {
         checkedBtn.removeAttribute('checked');
         let newCheckedBtn = document.getElementById('system');
         newCheckedBtn.setAttribute('checked', '');
 
-        newLeft = (btnBg.offsetWidth - btn.offsetWidth) / 2 - initShift;
-        btn.style.transform = `translate(${newLeft}px)`; 
+        moveToCenter(btnBg, btn, shiftX);
     } else if (checkedBtn.id === 'system') {
         checkedBtn.removeAttribute('checked');
         let newCheckedBtn = document.getElementById('light');
         newCheckedBtn.setAttribute('checked', '');
 
-        newLeft = btnBg.offsetWidth - btn.offsetWidth - initShift * 2;
-        btn.style.transform = `translate(${newLeft}px)`;
-    } else {
+        moveToRight(btnBg, btn, shiftX);
+    } else if (checkedBtn.id === 'light') {
         checkedBtn.removeAttribute('checked');
         let newCheckedBtn = document.getElementById('dark');
         newCheckedBtn.setAttribute('checked', '');
 
-        newLeft = 0;
-        btn.style.transform = `translate(${newLeft}px)`;
+        moveToLeft(btn);
     }
 }
+
+
+//moving toggle on label-clicking
+let fieldset = document.querySelector('.toggle');
+fieldset.addEventListener('click', onLabelClick);
+
+function onLabelClick(event) {
+    let target = event.target.closest('label');
+    if (!target || !fieldset.contains(target)) return;
+
+    let nextId = target.getAttribute('for');
+    document.getElementById(nextId).setAttribute('checked', '');
+
+    if (nextId == 'dark') {
+        moveToLeft(btn); 
+    } else if (nextId == 'system') {
+        moveToCenter(btnBg, btn, shiftX);
+    } else {
+        moveToRight(btnBg, btn, shiftX);
+    }
+}
+
+
+
+//helper functions
+function moveToCenter(bg, el, shiftX) {
+    let center = (bg.offsetWidth - el.offsetWidth) / 2 - shiftX;
+    el.style.transform = `translate(${center}px)`; 
+}
+
+function moveToRight(bg, el, shiftX) {
+    let leftEdge;
+    leftEdge = bg.offsetWidth - el.offsetWidth - shiftX * 2;
+    el.style.transform = `translate(${leftEdge}px)`;
+}
+
+function moveToLeft(el) {
+    el.style.transform = `translate(${0}px)`;
+}
+
+
+//focus-button event
+btn.addEventListener('focus', onButtonFocus);
+
+
+function onButtonFocus() {
+    document.addEventListener('keydown', arrowPressed);
+    btn.addEventListener('blur', function() {
+        document.removeEventListener('keydown', arrowPressed);
+    });
+
+    function arrowPressed(event) {
+        let checkedBtn = document.querySelector('input[checked]');
+
+        if (checkedBtn.id === 'dark' && event.key == 'ArrowRight') {
+            checkedBtn.removeAttribute('checked');
+            let newCheckedBtn = document.getElementById('system');
+            newCheckedBtn.setAttribute('checked', '');
+    
+            moveToCenter(btnBg, btn, shiftX);
+
+        } else if (checkedBtn.id === 'system' && event.key == 'ArrowRight') {
+            checkedBtn.removeAttribute('checked');
+            let newCheckedBtn = document.getElementById('light');
+            newCheckedBtn.setAttribute('checked', '');
+    
+            moveToRight(btnBg, btn, shiftX);
+
+        } else if (checkedBtn.id === 'system' && event.key == 'ArrowLeft') {
+            checkedBtn.removeAttribute('checked');
+            let newCheckedBtn = document.getElementById('dark');
+            newCheckedBtn.setAttribute('checked', '');
+    
+            moveToLeft(btn);
+
+        } else if (checkedBtn.id === 'light' && event.key == 'ArrowLeft') {
+            checkedBtn.removeAttribute('checked');
+            let newCheckedBtn = document.getElementById('system');
+            newCheckedBtn.setAttribute('checked', '');
+    
+            moveToCenter(btnBg, btn, shiftX);
+        } else {
+            return;
+        }
+    }
+}
+
